3cdb0f1f70a68c131a3eea493f83e5b902c6c18f
souleimarejeb <80159191+souleimarejeb@users.noreply.github.com>
2024-04-24 23:33:23 +0100

    4 commit

--- src/main/java/com/example/myjavafxapp/SendSms.java
+++ src/main/java/com/example/myjavafxapp/SendSms.java
@@ -7,11 +7,24 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
-
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.util.EncodingUtil;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 public class SendSms {
 
     @FXML
     private Button ClearButton;
+
+    public static String url_str;
 
     @FXML
     private Button Editbtn;
@@ -31,10 +44,56 @@ public class SendSms {
     @FXML
     private Pane pane_112;
 
+    public static void setvar (String argname, String argvalue) {
+
+
+
+        if (argname != null) {
+
+            if (argvalue != null) {
+
+                url_str = url_str + "&" + argname + "=";
+
+                try {
+
+                    String encoded = URLEncoder.encode (argvalue, "UTF-8");
+
+                    url_str = url_str + encoded;
+
+                }
+
+                catch (UnsupportedEncodingException e) {
+
+                    url_str = url_str + argvalue;
+
+                }
+
+            }
+
+        }
+
+
+
+    }
+
     @FXML
     void setCancelButtonIDAction(ActionEvent event) {
 
+
+        sendsms.server = "https://sample.smshosts.com/";
+
+        sendsms.user = "username";
+
+        sendsms.password = "password";
+
+        sendsms.phonenumber = "+9999999999";
+
+        sendsms.text = "This is a test message";
+
+        sendsms.send();
+
     }
+
 
 
 
